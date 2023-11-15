2f08e108c51df46ca7127127837633b52dfd564d
Nick Monteleone <nicholasmonteleone25@gmail.com>
2023-11-15 12:37:43 -0800

    request works

--- tvmaze.js
+++ tvmaze.js
@@ -4,6 +4,7 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
+const TV_MAZE_URL = "http://api.tvmaze.com";
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -12,21 +13,34 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+async function getShowsByTerm(searchInput) {
+  console.log('making request to api')
   // ADD: Remove placeholder & make request to TVMaze search shows API.
+  const params = new URLSearchParams({q: searchInput});
+  const response = await fetch(
+    // note: unable to have headers
+    `${TV_MAZE_URL}/search/shows?${params}`,
+    {
+      method: "GET",
+    });
+
+  const searchResult = (await response.json())[0];
+  console.log("first result:", searchResult);
+
+  // convert search result to hard coded format below
 
   return [
     {
       id: 1767,
       name: "The Bletchley Circle",
       summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
+        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
            women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
+         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+           normal lives, modestly setting aside the part they played in
+           producing crucial intelligence, which helped the Allies to victory
            and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
+           unsolved murder she is met by skepticism from the police. She
            quickly realises she can only begin to crack the murders and bring
            the culprit to justice with her former friends.</p>`,
       image:
@@ -48,9 +62,9 @@ function displayShows(shows) {
     const $show = $(`
         <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
-           <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+           <img
+              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
